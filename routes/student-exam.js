6aeb14b2560a86cb03e5f53e868546346c6a3822
carlojohn98 <johncarlomiel@gmail.com>
2019-05-10 19:00:01 +0800

    Updateeeeeeeeeeeeeeeeeeeeeeee

--- routes/student-exam.js
+++ routes/student-exam.js
@@ -21,61 +21,83 @@ router.get('/student-exam/questions/:exam_id/:std_id', (req, res) => {
 });
 
 router.patch('/student-exam/submit/:std_id', (req, res) => {
-
+   let counter = 0;
+   let updates = "";
    fs.readFile(req.body.code_path, 'utf8', (err, data) => {
       if (err) throw err;
       let sql = "SELECT * FROM student_cases INNER JOIN test_cases ON student_cases.test_id = test_cases.test_id WHERE test_cases.qts_id = ? AND student_cases.std_id = ?";
       pool.getConnection((err, connection) => {
          if (err) throw err;
 
-         let query = connection.query(sql, [req.body.qts_id, req.params.std_id], (err, results) => {
-            let counter = 0;
-            let updates = "";
-            results.forEach(async (element, index) => {
+         let query = connection.query(sql, [req.body.qts_id, req.params.std_id], async (err, results) => {
+            for (let index = 0; index < results.length; index++) {
+
                try {
                   let input = results[index].inputs.replace(/\\n/g, '\n');
                   let checkerFile = await java.runSource(data, { stdin: `${input}` });
-                  let userFile = await java.runSource(req.body.code, { stdin: `${input}` });
-                  if (checkerFile.stdout == userFile.stdout) {
+                  let userFile = await java.runSource(req.body.code, { stdin: `${input}` })
+
+                  if (checkerFile.stdout != "" && userFile.stdout != "" && checkerFile.stdout.trim() === userFile.stdout.trim()) {
+                     console.log("tama")
                      counter++;
-                     updates += (`UPDATE student_cases SET isCorrect = true WHERE std_id = ${req.params.std_id} AND test_id = ${element.test_id};`);
+                     updates += `UPDATE student_cases SET err_msg = "", isCorrect = true WHERE std_id = ${req.params.std_id} AND test_id = ${results[index].test_id};`;
+
                   } else {
-                     updates += (`UPDATE student_cases SET isCorrect = false WHERE std_id = ${req.params.std_id} AND test_id = ${element.test_id};`);
+                     updates += `UPDATE student_cases SET err_msg = ${pool.escape(userFile.stderr)},isCorrect = false WHERE std_id = ${req.params.std_id} AND test_id = ${results[index].test_id};`;
                      console.log('mali')
+
+
                   }
-                  console.log(counter)
+
+                  if (index == results.length - 1) {
+
+                     if (counter == results.length) {
+                        updates += `UPDATE student_question SET isCompleted = true,
+                         code = ${pool.escape(req.body.code)} WHERE std_id = ${req.params.std_id}
+                          AND qts_id = ${req.body.qts_id};`;
+                     } else {
+                        updates += `UPDATE student_question SET isCompleted = false,
+                         code = ${pool.escape(req.body.code)}
+                          WHERE std_id = ${req.params.std_id} AND qts_id = ${req.body.qts_id};`;
+                     }
+                     callback(updates);
+
+                  }
 
                } catch (error) {
                   console.log(error)
                }
-               if (index == results.length - 1) {
-                  if (counter == results.length - 1) {
-                     updates += (`UPDATE student_question SET isCompleted = true, code = '${req.body.code}' WHERE std_id = ${req.params.std_id} AND qts_id = ${req.body.qts_id};`);
-                  } else {
-                     updates += (`UPDATE student_question SET isCompleted = false, code = '${req.body.code}' WHERE std_id = ${req.params.std_id} AND qts_id = ${req.body.qts_id};`);
 
-                  }
-                  callback(updates);
-               }
-            });
+
+            }
+
+            // results.forEach(async (results[index], index) => {
+
+
+            // });
 
 
 
 
 
          });
+
          function callback(updates) {
-            console.log(updates)
-            connection.query(updates, (err, results) => {
+            console.log(req.params.std_id)
+            console.log("tapos na")
+            let query = connection.query(updates, (err, results) => {
                if (err) {
                   connection.release();
                   res.json(err);
                }
 
                connection.release();
-               res.json(results);
+               res.json(results).end();
+
             })
+
          }
+
       });
 
 
@@ -83,6 +105,8 @@ router.patch('/student-exam/submit/:std_id', (req, res) => {
 
 
 });
+
+
 
 
 module.exports = router;
--- routes/student.js
+++ routes/student.js
@@ -53,6 +53,29 @@ router.patch('/student/:std_id', [verifyToken, isAdmin], (req, res) => {
 });
 
 
+router.get('/student-questions/:std_id', (req, res) => {
+   let sql = "SELECT * FROM student_question LEFT JOIN questions ON student_question.qts_id = questions.qts_id WHERE student_question.std_id = ?";
+   pool.query(sql, [req.params.std_id], (err, results) => {
+      if (err) res.json(err);
+      res.json(results);
+   });
+});
+
+router.get('/student-test-cases/:std_id/:qts_id', (req, res) => {
+   let sql = "SELECT err_msg,inputs,isCorrect,isHidden,points FROM student_cases INNER JOIN test_cases ON student_cases.test_id = test_cases.test_id WHERE student_cases.std_id = ? AND test_cases.qts_id = ?";
+   pool.query(sql, [req.params.std_id, req.params.qts_id], (err, results) => {
+      if (err) res.json(err);
+
+      results.forEach((element, index) => {
+         if (element.isHidden) {
+            results[index]["inputs"] = "hidden";
+            results[index]["err_msg"] = "hidden";
+         }
+      });
+      res.json(results);
+   });
+});
+
 
 
 
